2d2514c5c878b1637a8532225f537244183b987f
Albert <albertsoares97@gmail.com>
2023-05-23 18:12:05 -0300

    primeiras estilizações

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,7 +1,18 @@
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
 const BarraLateral = () => (
-  <aside>
-    <h1>Barra lateral</h1>
-  </aside>
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
 )
 
 export default BarraLateral
